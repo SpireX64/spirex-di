766572bd44f463dc02381d298f8d5b2cd2c50d64
SpireX <SpireX@outlook.com>
2024-12-23 21:11:04 +0200

    chore: Initial commit

---
+++ packages/di/jest.config.js
@@ -0,0 +1,19 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+    displayName: "@spirex/di",
+    preset: "ts-jest",
+    testEnvironment: "node",
+    clearMocks: true,
+    testTimeout: 200,
+    coverageDirectory: "<rootDir>/coverage",
+    coveragePathIgnorePatterns: [
+        "<rootDir>/build/",
+        "<rootDir>/node_modules/",
+        "<rootDir>/types/",
+    ],
+    testPathIgnorePatterns: [
+        "<rootDir>/build/",
+        "<rootDir>/node_modules/",
+        "<rootDir>/types/",
+    ],
+};
