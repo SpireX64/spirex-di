2820b2678e7ade134eb31d9d02a73f283d273ebb
SpireX <SpireX@outlook.com>
2024-12-24 10:06:16 +0200

    test(builder): create instance of container builder

--- packages/di/src/DIContainerBuilder.test.ts
+++ packages/di/src/DIContainerBuilder.test.ts
@@ -1,1 +1,9 @@
-describe("DIContainerBuilder", () => {});
+import { DIContainerBuilder } from "./DIContainerBuilder";
+
+describe("DIContainerBuilder", () => {
+    test("Create builder instance", () => {
+        const builder = new DIContainerBuilder();
+
+        expect(builder).toBeInstanceOf(DIContainerBuilder);
+    });
+});
