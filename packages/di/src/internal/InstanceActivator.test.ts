acccf725daac695bf33e41e4e56cf2164301d5da
SpireX <SpireX@outlook.com>
2025-01-04 11:41:45 +0200

    feat(activator): Dependencies cycle detector

--- packages/di/src/internal/InstanceActivator.test.ts
+++ packages/di/src/internal/InstanceActivator.test.ts
@@ -2,8 +2,11 @@ import {
     createMockResolver,
     makeFactoryEntryMock,
     makeInstanceEntryMock,
+    makeRegistrar,
 } from "../__test__/mocks";
 import { InstanceActivator } from "./InstanceActivator";
+import { DIContainer } from "../DIContainer";
+import { catchError } from "../__test__/errors";
 
 describe("InstanceActivator", () => {
     test("Activate instance entry", () => {
@@ -56,5 +59,107 @@ describe("InstanceActivator", () => {
         // eslint-disable-next-line @typescript-eslint/unbound-method
         expect(resolver.get).toHaveBeenCalled();
         expect(factoryEntry.factory).toHaveBeenCalledTimes(1);
+    });
+
+    test("Activation short cycle", () => {
+        type TypeMap = { value: string };
+
+        // Arrange --------
+        const registrar = makeRegistrar(
+            makeFactoryEntryMock<TypeMap>(
+                "value",
+                (r) => r.get("value"),
+                "lazy",
+            ),
+        );
+        const activator = new InstanceActivator<TypeMap>();
+        const container = new DIContainer(registrar, activator);
+
+        // Act ---------
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        const entry = registrar.findTypeEntry("value")!;
+        const error = catchError(() => {
+            activator.createInstance(entry, container);
+        });
+
+        // Assert ------
+        expect(error).not.toBeUndefined();
+        expect(error?.message).toContain("value -> value");
+    });
+
+    test("Activation long cycle", () => {
+        type TypeMap = { valueA: string; valueB: string; valueC: string };
+
+        // Arrange --------
+        const registrar = makeRegistrar(
+            makeFactoryEntryMock<TypeMap>(
+                "valueA",
+                (r) => r.get("valueB"),
+                "lazy",
+            ),
+            makeFactoryEntryMock<TypeMap>(
+                "valueB",
+                (r) => r.get("valueC"),
+                "lazy",
+            ),
+            makeFactoryEntryMock<TypeMap>(
+                "valueC",
+                (r) => r.get("valueA"),
+                "lazy",
+            ),
+        );
+        const activator = new InstanceActivator<TypeMap>();
+        const container = new DIContainer(registrar, activator);
+
+        // Act ---------
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        const entry = registrar.findTypeEntry("valueA")!;
+        const error = catchError(() => {
+            activator.createInstance(entry, container);
+        });
+
+        // Assert ------
+        expect(error).not.toBeUndefined();
+        expect(error?.message).toContain(
+            "[valueA] -> valueB -> valueC -> [valueA]",
+        );
+    });
+
+    test("Deep activation cycle", () => {
+        type TypeMap = { valueA: string; valueB: string; valueC: string };
+
+        // Arrange --------
+        const registrar = makeRegistrar(
+            makeFactoryEntryMock<TypeMap>(
+                "valueA",
+                (r) => r.get("valueB"),
+                "lazy",
+            ),
+            makeFactoryEntryMock<TypeMap>(
+                "valueB",
+                (r) => r.get("valueC"),
+                "lazy",
+            ),
+            makeFactoryEntryMock<TypeMap>(
+                "valueC",
+                (r) => r.get("valueB"),
+                "lazy",
+            ),
+        );
+        const activator = new InstanceActivator<TypeMap>();
+        const container = new DIContainer(registrar, activator);
+
+        // Act ---------
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        const entry = registrar.findTypeEntry("valueA")!;
+        const error = catchError(() => {
+            activator.createInstance(entry, container);
+        });
+
+        // Assert ------
+        expect(error).not.toBeUndefined();
+        expect(error?.message).toContain(
+            "valueA -> [valueB] -> valueC -> [valueB]",
+        );
     });
 });
--- packages/di/src/internal/InstanceActivator.ts
+++ packages/di/src/internal/InstanceActivator.ts
@@ -1,12 +1,45 @@
 import type { IInstanceResolver, TTypeEntry, TTypeMapBase } from "../types";
-import { isFactoryTypeEntry } from "../utils";
+import { isInstanceTypeEntry } from "../utils";
+
+const Errors = {
+    DependenciesCycle: (entryType: string, chain: readonly string[]) => {
+        const sep = " -> ";
+        const chainString =
+            chain.length > 2
+                ? chain
+                      .map((it) => (it === entryType ? `[${it}]` : it))
+                      .join(sep)
+                : entryType + sep + entryType;
+        return `Activation failed: A cyclic dependency was detected while resolving type '${entryType}'. Ensure that your dependencies do not form a circular reference. (Activation chain: ${chainString})`;
+    },
+};
 
 export class InstanceActivator<TypeMap extends TTypeMapBase> {
+    private _currentActivationStack: string[] = [];
+
     public createInstance<Key extends keyof TypeMap>(
         entry: TTypeEntry<TypeMap, Key>,
         resolver: IInstanceResolver<TypeMap>,
     ): TypeMap[Key] {
-        if (isFactoryTypeEntry(entry)) return entry.factory(resolver);
-        return entry.instance;
+        if (isInstanceTypeEntry(entry)) return entry.instance;
+
+        const entryType = entry.type.toString();
+        const hasDependencyCycle =
+            this._currentActivationStack.includes(entryType);
+        this._currentActivationStack.push(entryType);
+        if (hasDependencyCycle) {
+            const error = new Error(
+                Errors.DependenciesCycle(
+                    entryType,
+                    this._currentActivationStack,
+                ),
+            );
+            this._currentActivationStack.length = 0;
+            throw error;
+        }
+
+        const instance = entry.factory(resolver);
+        this._currentActivationStack.pop();
+        return instance;
     }
 }
