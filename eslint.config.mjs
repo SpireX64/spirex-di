a935e834ab0a8ba9a4e45f0f82cbc95df7bd8a79
SpireX <SpireX@outlook.com>
2025-03-27 00:53:42 +0200

    chore: allow namespaces in eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,20 @@
 // @ts-check
 
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
     eslint.configs.recommended,
     ...tseslint.configs.strictTypeChecked,
     {
+        rules: { "@typescript-eslint/no-namespace": "off" },
+    },
+    {
         languageOptions: {
             parserOptions: {
                 project: true,
-                tsconfigRootDir: import.meta.dirname
-            }
-        }
-    }
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+    },
 );
