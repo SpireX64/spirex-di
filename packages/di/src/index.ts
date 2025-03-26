440cd77efac4ae02ea4b105a1c4b0c1a53d6768b
SpireX <SpireX@outlook.com>
2025-03-27 01:11:03 +0200

    feat: prepare index file for exports

---
+++ packages/di/src/index.ts
@@ -0,0 +1,48 @@
+import { TTypeMapBase } from "./types";
+import { DIContainerBuilder } from "./DIContainerBuilder";
+import type { TAnyDIModule, TDynamicImportDelegate } from "./modules/types";
+import { dynamicModuleFactory } from "./modules/dynamicModuleFactory";
+import { staticModuleFactory } from "./modules/staticModuleFactory";
+
+export {
+    type TAnyDIModule,
+    type TDynamicImportDelegate,
+    staticModuleFactory,
+    dynamicModuleFactory,
+    DIContainerBuilder,
+};
+
+export type * from "./modules/types";
+export type * from "./types";
+
+export {
+    isInstanceTypeEntry,
+    isFactoryTypeEntry,
+    compareLifecycles,
+    checkIsDisposable,
+    checkIsPhantomInstance,
+    unwrapPhantom,
+} from "./utils";
+
+export namespace DI {
+    export const builder = <TypeMap extends TTypeMapBase>() =>
+        new DIContainerBuilder<TypeMap>();
+
+    export function module(
+        name: string,
+    ): ReturnType<typeof staticModuleFactory>;
+
+    export function module<ESModule>(
+        name: string,
+        importDelegate: TDynamicImportDelegate<ESModule>,
+    ): ReturnType<typeof dynamicModuleFactory<ESModule>>;
+
+    export function module<ESModule>(
+        name: string,
+        importDelegate?: TDynamicImportDelegate<ESModule>,
+    ) {
+        return importDelegate
+            ? dynamicModuleFactory(name, importDelegate)
+            : staticModuleFactory(name);
+    }
+}
