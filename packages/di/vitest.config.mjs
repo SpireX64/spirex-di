338aa286a885375d75b359ecf9a5044d6a4fa7b4
Artem Sobolenkov <SpireX@outlook.com>
2026-06-17 14:34:32 +0200

    fix: vitest coverage config

--- packages/di/vitest.config.mjs
+++ packages/di/vitest.config.mjs
@@ -5,18 +5,19 @@ export default defineConfig({
     test: {
         name: "@spirex/di",
         root: "./src",
-        include: ["**/*.{test,spec}.(c|m)js"],
+        include: ["**/*.{test,spec}.{js,cjs,mjs}"],
         exclude: [
             "**/node_modules/**",
             "**/.{idea,git,cache,output,temp}/**",
             "**/{rollup,vitest,eslint,prettier}.config.*",
             "**/{dist,coverage}/**",
-            "**/__test__/*.{test,spec}.(c|m)js",
+            "**/__test__/*.{test,spec}.{js,cjs,mjs}",
         ],
         coverage: {
             provider: "istanbul",
             reporter: ["text", "lcov", "html"],
             reportsDirectory: "../coverage",
+            exclude: ["**/*.{test,spec}.{js,cjs,mjs}", "**/__tests__/**"],
         },
         testTimeout: 200,
         clearMocks: true,
